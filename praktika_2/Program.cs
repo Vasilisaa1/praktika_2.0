96608d5c6a49ff5b7a2d36284563275df4dda402
qwelord <secondofall22@rambler.ru>
2025-11-19 16:52:33 +0500

    метод «Send» Veber

--- praktika_2/Program.cs
+++ praktika_2/Program.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using Common;
+using Newtonsoft.Json;
 
 namespace praktika_2
 {
     internal class Program
     {
-        public static List<Leaders> Leaders = new List<Leaders>;
+        public static List<Leaders> Leaders = new List<Leaders>();
         public static List<ViewModelUserSettings> remoteIPAddress =new List<ViewModelUserSettings>();
         public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort= 5001;
@@ -17,5 +21,34 @@ namespace praktika_2
         static void Main(string[] args)
         {
         }
+        private static void Send()
+        {
+            foreach (ViewModelUserSettings User in remoteIPAddress) {
+                UdpClient sender= new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(User.IPAddress), int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+                    sender.Send(bytes, bytes.Length, endPoint);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение:" + ex.ToString() + "\n" + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+
+
+                }
+
+
+            }
+
+            }
     }
 }
