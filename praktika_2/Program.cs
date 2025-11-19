4260ab952fd0cce3bbf350347f5bb6d1546e667d
qwelord <secondofall22@rambler.ru>
2025-11-19 16:27:30 +0500

    Добавленные перменные. Вебер

--- praktika_2/Program.cs
+++ praktika_2/Program.cs
@@ -8,6 +8,12 @@ namespace praktika_2
 {
     internal class Program
     {
+        public static List<Leaders> Leaders = new List<Leaders>;
+        public static List<ViewModelUserSettings> remoteIPAddress =new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        private static int localPort= 5001;
+        public static int MaxSpeed = 15;
+
         static void Main(string[] args)
         {
         }
