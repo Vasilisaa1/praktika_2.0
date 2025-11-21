5b61e239bfb0ec2f6fc9fedfd2afc5ce39f9f48d
vasilisaa <vasilisaveber7@gmail.com>
2025-11-21 14:49:04 +0500

    страницы «MainWindow». Veber

--- Downloads/praktika_2.0-master/praktika_2.0-master/SnakeWpf/MainWindow.xaml.cs
+++ Downloads/praktika_2.0-master/praktika_2.0-master/SnakeWpf/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Security.Cryptography;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Common;
 
 namespace SnakeWpf
 {
@@ -20,6 +25,33 @@ namespace SnakeWpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary> Главное окно, используется для для общения между страницами
+        public static MainWindow mainWindow;
+
+        /// <summary> Модель данных для передачи IP адреса устройства, порта и никнейма
+        public ViewModelUserSettings ViewModelUserSettings = new ViewModelUserSettings();
+
+        /// <summary> Модель игрока в который передаются координаты змеи, яблок и точки
+        public ViewModelGames ViewModelGames = null;
+
+        /// <summary> Удалённый IP адрес для подключения к серверу
+        public static IPAddress remoteIPAddress = IPAddress.Parse("127.0.0.1");
+
+        /// <summary> Удалённый порт для подключения к серверу
+        public static int remotePort = 5001;
+
+        /// <summary> Основной поток для получения данных о игре
+        public Thread tRec;
+
+        /// <summary> UDP клиент для получения данных
+        public UdpClient receivingUdpClient;
+
+        /// <summary> Страница HOME
+        public SnakeWPF.Pages.Home Home = new SnakeWPF.Pages.Home();
+
+        
+        public SnakeWPF.Pages.Game Game = new SnakeWPF.Pages.Game();
+
         public MainWindow()
         {
             InitializeComponent();
